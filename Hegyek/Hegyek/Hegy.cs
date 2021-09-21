b71d3331b846b28bfeea5d013f214378d18e5339
mark199850 <46360948+mark199850@users.noreply.github.com>
2021-09-21 10:30:33 +0200

    Add files via upload

---
+++ Hegyek/Hegyek/Hegy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hegyek
+{
+    class Hegy
+    {
+        public string csucsnev { get; private set; }
+        public string hegy { get; private set; }
+        public int magassag { get; private set; }
+
+        public Hegy(string sor) //konstruktor: nincs visszatérési értéke, a neve ugyan az mint az osztálynak
+        {
+            string[] darabol = sor.Split(';');
+            this.csucsnev = darabol[0]; //this. -ot is eléjük lehet írni
+            hegy = darabol[1];
+            magassag = Convert.ToInt32(darabol[2]);
+        }
+
+    }
+}
---
+++ Hegyek/Hegyek/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hegyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hegy> lista = new List<Hegy>();
+            StreamReader sr = new StreamReader("hegyekMo.txt");
+            sr.ReadLine();
+
+            while (!sr.EndOfStream) //sr.Peek()
+            {
+                lista.Add(new Hegy(sr.ReadLine()));
+            }
+            //Feladat 1
+            Console.WriteLine("Feladat 1: "+lista.Count);
+            
+            //Feladat 2
+            double sum = 0;
+            foreach (var item in lista)
+            {
+                sum = sum + item.magassag;
+            }
+
+            Console.WriteLine("2. Feladat: " + sum / lista.Count);
+
+            //Feladat 3
+
+            int max = lista[0].magassag;
+            int id = 0;
+            for (int i = 1; i < lista.Count; i++)
+            {
+                if(lista[i].magassag > max)
+                {
+                    max = lista[i].magassag;
+                    id = i;
+                }
+            }
+
+            Console.WriteLine("3. Feladat :"+"\n\t" +lista[id].csucsnev+"\n\t"+lista[id].hegy+"\n\t"+lista[id].magassag+" m");
+
+
+
+            //Feladat 4
+            int m;
+            Console.Write("Kérek egy egész számot: ");
+            m = int.Parse(Console.ReadLine());
+            bool van = false;
+            int id2 = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].magassag > m)
+                {
+                    van = true;
+                    id2 = i;
+                    break;
+                }
+            }
+            if (van)
+            {
+                Console.WriteLine("Van magasabb csúcs: {0}",lista[id2].csucsnev);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
