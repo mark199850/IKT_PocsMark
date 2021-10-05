0d48ba23704363b9debbe435e2f283194facce4e
mark199850 <mark199850@gmail.com>
2021-10-05 09:22:03 +0200

    Űrlap validálás osztályszerkezete kialakítva

---
+++ Projekt1/classes.php
@@ -0,0 +1,16 @@
+<?php
+class Forms{
+    protected $uName;
+    protected $uPass;
+    protected $uNameErr;
+    protected $uPassErr;
+}
+
+class LoginCheck extends Forms {
+
+}
+
+class RegCheck extends Forms {
+
+}
+?>
